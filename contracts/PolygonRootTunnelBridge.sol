637b763f0f2c2ee037a608174b00ddb13492623c
donosonaumczuk <adonoso@itba.edu.ar>
2022-06-13 02:35:20 +0100

    Polygon bridge refactored to follow bridge interface

--- contracts/PolygonRootTunnelBridge.sol
+++ contracts/PolygonRootTunnelBridge.sol
@@ -1,58 +1,82 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@maticnetwork/fx-portal/contracts/tunnel/FxBaseRootTunnel.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./interfaces/IUBI.sol";
-import "./interfaces/IFUBI.sol";
+import '@maticnetwork/fx-portal/contracts/tunnel/FxBaseRootTunnel.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './interfaces/IUBI.sol';
+import './interfaces/IFUBI.sol';
+import './interfaces/IBridge.sol';
 
-contract UBI2PolygonRootTunnel is IERC721Receiver, FxBaseRootTunnel, ReentrancyGuard, Ownable {
-    address ubi;
-    address fubi;
+contract PolygonRootTunnelBridge is IBridge, FxBaseRootTunnel, Ownable {
+    address UBI;
+    address fUBI;
+    address bridgeRouter;
     bytes public latestData;
-    
-    bytes32 public constant FUBI_DEPOSIT = keccak256("FUBI_DEPOSIT");
-    
+
+    bytes32 public constant FUBI_DEPOSIT = keccak256('FUBI_DEPOSIT');
+
     /// @dev Event emited when a FUBI has been deposited to the bridge.
     event FUBIDeposited(address indexed sourceHuman, address indexed depositer, uint256 id);
 
-    constructor(address pUBI, address pFUBI, address _checkpointManager, address _fxRoot) FxBaseRootTunnel(_checkpointManager, _fxRoot) {
-        ubi = pUBI;
-        fubi = pFUBI;
+    constructor(
+        address _UBI,
+        address _fUBI,
+        address _checkpointManager,
+        address _fxRoot
+    ) FxBaseRootTunnel(_checkpointManager, _fxRoot) {
+        UBI = _UBI;
+        fUBI = _fUBI;
     }
 
     /// @dev set the UBI address.
-    function setUBI(address pUBI) public onlyOwner {
-        ubi = pUBI;
+    function setUBI(address _UBI) public onlyOwner {
+        UBI = _UBI;
     }
 
     /// @dev set the FUBI address.
-    function setFUBI(address pFUBI) public onlyOwner {
-        fubi = pFUBI;
+    function setFUBI(address _fUBI) public onlyOwner {
+        fUBI = _fUBI;
     }
 
     function _processMessageFromChild(bytes memory data) internal override {
-        
+        //TODO: Implement later
     }
 
-    /// @dev Callabck for when an ERC721 tokenm is received. Caller must be FUBI contract.
-    function onERC721Received(address, address, uint256 _tokenId, bytes calldata _data) public virtual override nonReentrant returns (bytes4) {
-        require(IUBI(ubi).isDelegator(msg.sender), "only delegator can deposit");
-        require(msg.sender == fubi, "sender must be fubi");
-        (uint256 ratePerSecond, // The rate of UBI to drip to this Flow from the current accrued value
-        uint256 startTime,
-        address sender,
-        bool isActive) = IFUBI(fubi).getFlow(_tokenId);
-         require(isActive, "can't bridge inactive flow");
+    function bridgeAmount(
+        uint256 chainId,
+        uint256 amount,
+        bytes calldata data
+    ) external override returns (bool) {
+        // TODO: Implement later
+    }
 
-        // Source (will be recipient), rate, time the the flow was delegated to the bridge.
-         bytes memory message = abi.encode(FUBI_DEPOSIT, abi.encode(sender, ratePerSecond, block.timestamp, _tokenId));
-         _sendMessageToChild(message);
+    function bridgeFlow(
+        uint256 chainId,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override returns (bool) {
+        // require(IUBI(ubi).isDelegator(msg.sender), 'only delegator can deposit');
+        if (msg.sender != bridgeRouter) {
+            revert('OnlyBridgeRouter');
+        }
+        // NOTE: BirdgeRouter already validated that the call comes from Flow owner or fUBI contract.
+        (uint256 ratePerSecond, uint256 startTime, address sender, bool isActive) = IFUBI(fUBI).getFlow(tokenId);
+        if (!isActive) {
+            revert('InactiveFlow');
+        }
+        // TODO: Add origin chainId in the message, to avoid problems in case we have more bridges
+        bytes memory message = abi.encode(FUBI_DEPOSIT, abi.encode(sender, ratePerSecond, block.timestamp, tokenId));
+        _sendMessageToChild(message);
+        emit FUBIDeposited(sender, msg.sender, tokenId);
+        return true;
+    }
 
-         emit FUBIDeposited(sender, msg.sender, _tokenId);
-        
-        return this.onERC721Received.selector;
+    function _getChainId() internal view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
     }
 }
