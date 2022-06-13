dbd4158b11f7610015a4c124d0d0fa41f294a9f3
donosonaumczuk <adonoso@itba.edu.ar>
2022-06-13 02:32:17 +0100

    Bridge router improved

--- contracts/BridgeRouter.sol
+++ contracts/BridgeRouter.sol
@@ -1,12 +1,21 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import {IERC721Receiver} from '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
 import {IBridge} from './interfaces/IBridge.sol';
+import {IERC721} from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
-contract BridgeRouter {
+// TODO: Make the BridgeRouter a TransparentUpgradeableProxy so it could support future use cases.
+// TODO: Also consider renaming it to BridgeManager.
+contract BridgeRouter is IERC721Receiver {
     error OnlyGovernance();
     error UnsupportedChain();
     error InvalidAddress();
+
+    //TODO: improve event signatures
+    event FlowBridged();
+    event AmountBridged();
 
     modifier onlyGovernance() {
         if (msg.sender == governance) {
@@ -17,8 +26,22 @@ contract BridgeRouter {
     }
 
     address governance;
+    address UBI;
+    address fUBI;
 
     mapping(uint256 => address) bridgeByChainId;
+
+    constructor(
+        address _governance,
+        address _UBI,
+        address _fUBI
+    ) {
+        governance = _governance;
+        UBI = _UBI;
+        fUBI = _fUBI;
+    }
+
+    //TODO: Should we add EIP-712 meta-tx support for bridge functions?
 
     /**
      * @notice Sets the
@@ -45,21 +68,52 @@ contract BridgeRouter {
         uint256 amount,
         bytes calldata data
     ) external {
+        IERC20(UBI).transferFrom(msg.sender, address(this), amount); //TODO: Worth to use SafeERC20 with a fixed impl?
         _getBridgeIfSupported(chainId).bridgeAmount(chainId, amount, data);
+        emit AmountBridged();
     }
 
     /**
      * @dev Bridges a flow by routing the call to the corresponding bride implementation.
      * @param chainId the destination chain ID, specially in case same bridge can handle more than one.
-     * @param accrualPerSecond the accrual of the flow to brige.
+     * @param tokenId the token ID of the flow NFT.
      * @param data arbitrary data that might be required by the bridge implementation.
      */
     function bridgeFlow(
         uint256 chainId,
-        uint256 accrualPerSecond,
+        uint256 tokenId,
         bytes calldata data
     ) external {
-        _getBridgeIfSupported(chainId).bridgeFlow(chainId, accrualPerSecond, data);
+        //TODO: Could use safeTransferFrom and let the onERC721Received do the magic, but this should be cheaper
+        IERC721(fUBI).transferFrom(msg.sender, address(this), tokenId);
+        _bridgeFlow(chainId, tokenId, data);
+    }
+
+    /**
+     * @param data it should contain encoded the chainId and then any arbitrary data that might be required by the
+     * bridge implementation.
+     */
+    function onERC721Received(
+        address, /* operator */
+        address, /* from */
+        uint256 tokenId,
+        bytes calldata data
+    ) public virtual override returns (bytes4) {
+        if (msg.sender == fUBI) {
+            (uint256 chainId, bytes memory bridgeData) = abi.decode(data, (uint256, bytes));
+            _bridgeFlow(chainId, tokenId, bridgeData);
+        }
+        // TODO: Maybe add else branch with revert to be firendly against accidental unsupported-NFTs transfers?
+        return this.onERC721Received.selector;
+    }
+
+    function _bridgeFlow(
+        uint256 chainId,
+        uint256 tokenId,
+        bytes memory data
+    ) internal {
+        _getBridgeIfSupported(chainId).bridgeFlow(chainId, tokenId, data);
+        emit FlowBridged();
     }
 
     /**
